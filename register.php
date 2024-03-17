35c31dcc66a0e01fcdd4fd4edc1b720bd3ff5aca
Shanley Sebial <110329841+ShunMeizz@users.noreply.github.com>
2024-03-17 13:03:59 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php
     include 'connect.php';
-    $notUnique = false;
+
+    $userNameUnique = true; //for the message box
+
     if(isset($_POST['btnRegister'])){		
 	
       $fname=$_POST['inputFname'];		
@@ -21,19 +23,14 @@
       if($row == 0){
           $sql ="Insert into tbluseraccount(emailadd,username,password) values('".$email."','".$username."','".$password."')";
           mysqli_query($connection,$sql);
-          echo "<script language='javascript'>
-          alert('Username is unique');
-    </script>";
           //also save data to tbluserprofile			
           $sql1 ="Insert into tbluserprofile(firstname,lastname,address,birthdate,gender,phonenumber) values('".$fname."','".$lname."','".$address."','".$bday."','".$gender."','".$phonenum."')";
           mysqli_query($connection,$sql1);
-  
-          echo "<script language='javascript'>
-                      alert('New record saved.');
-                </script>";
+
+          echo "<script>alert('New Record Saved')</script>";
           header("Location: login.php");
       }else{
-        $notUnique = true;
+        $userNameUnique = false;
       }  
     }
   
@@ -42,7 +39,7 @@
 <html>
     <head>
         <title>Gaklat Books Store</title>
-        <link href="css/Registration_Login.css" type="text/css" rel="stylesheet"/>
+        <link rel="stylesheet" href="css/Registration_Login.css" />
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
         <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Pacifico&family=Poppins&family=Rammetto+One&family=Zilla+Slab:wght@600&display=swap" rel="stylesheet">
       </head>
@@ -68,58 +65,63 @@
                     <form method ="post">
                       <h2>Register</h2>
                       <!-- Register Form Start -->
-                        <!-- First Name Input -->  
-                        <div class="input_box">
-                            <input type="text" input name="inputFname" placeholder="Enter your firstname" required />
+                        <!-- First Name, Last Name and Gender Input -->  
+                        <div class="input_box1">
+                            <input type="text" input name="inputFname" placeholder="Firstname" required />
+                            <input type="text" input name="inputLname" placeholder="Lastname" required />
+                            <select name="inputGender" required>
+                            <option value="" disabled selected>Gender</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                          </select>
                         </div>
-                        <!-- Last Name Input -->  
-                        <div class="input_box">
-                            <input type="text" input name="inputLname" placeholder="Enter your lastname" required />
-                        </div>
-                        <!-- Gender Input -->  
-                        <div class="input_box">
-                            <input type="text" input name="inputGender" placeholder="Enter your gender" required />
-                        </div>
-                        <span class="error_message"> Some text</span>
-                       <!-- UserName Input -->  
+                        <!-- UserName Input -->  
                        <div class="input_box">
-                          <input type="text" input name="inputUname" placeholder="Enter your username" required />
-                        <?php if ($notUnique): ?>
-                            <span class="error_message">Username already exist. Try a different username or Log in.</span>
-                        <?php endif; ?>
+                          <input type="text" input name="inputUname" placeholder="Username" required />
+                          <?php if (!$userNameUnique): ?>
+                            <span class="error_message">Username already exists. Try a different username or log in.</span>
+                          <?php endif; ?>
+                          <i class="uil uil-user user"></i>
                       </div>
-                      <!-- Email Input -->  
-                      <div class="input_box">
-                        <input type="email" input name="inputEmail" placeholder="Enter your email" required />
+                       <!-- Email Input -->  
+                       <div class="input_box">
+                        <input type="email" input name="inputEmail" placeholder="Email" required />
                         <i class="uil uil-envelope-alt email"></i>
                       </div>
                        <!-- Address Input -->  
                        <div class="input_box">
-                        <input type="text" input name="inputAddress" placeholder="Enter your address" required />
-                      
+                        <input type="text" input name="inputAddress" placeholder="Address" required />
+                        <i class="uil uil-location-point address"></i>
                       </div>
-                       <!-- Birthdate Input -->  
+                        <!-- Birthdate Input -->  
                        <div class="input_box">
-                        <input type="date" input name="inputBday" placeholder="Enter your birthdate" required />                     
+                        <input type="date" input name="inputBday" id="inputBday" placeholder="Birthdate" required />       
+                        <i class="uil uil-calendar-alt calendar"></i>              
                       </div>
                       <!-- PhoneNumber Input -->  
                       <div class="input_box">
-                            <input type="text" input name="inputPhoneNum" placeholder="Enter your phone number" required />
-                        </div>
+                            <input type="tel" input name="inputPhoneNum" placeholder="Phone number"  pattern="\d{11}" title="Please enter exactly 11 digits" required/>
+                            <i class="uil uil-phone phone"></i>
+                      </div>
+                     
+                       
                       <!-- Password Input -->
                       <div class="input_box">
-                        <input type="password" input name="inputPassword" placeholder="Enter your password" required />
+                        <input type="password" input name="inputPassword" placeholder="Password" required />
                         <i class="uil uil-lock password"></i>
-                        <i class="uil uil-eye-slash pw_hide"></i>
                       </div>
           
                       <!-- Register Button -->
                       <input type="submit" name="btnRegister" class="button" value="Register">
+                      <!-- Login Link -->
+                      <div class="login_signup">Already have an account? <a href="login.php" id="login">Login</a></div>
 
                     </form>
                 </div>
             </div>
         </div>
+       
     </body>
 </html>
 
